2c8b3dbe1ce21236999c9567d00490d383e551c4
NEHASHAH <nehashah123@users.noreply.github.com>
2018-11-30 19:00:10 -0500

    Cleaned up and commented code

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
 // new WOW().init();
+//creating atuomatic scrolling on all project pages
 if ($("#section1") != null){
 $('html, body').animate({
   scrollTop: $("#section1").offset().top
@@ -8,6 +9,7 @@ $('html, body').animate({
 };
 });
 
+//adding tracking to internal nav on each project page
 $('body').scrollspy({ target: '#internalnav' });
 $('ul.nav li a:not(.sidenavlink)').bind('click', function(e) {
   //e.preventDefault();
